b1a0894957a10ebcef6729403c23587d216b4cc2
lukasz <lorzechowski@protonmail.com>
2019-09-17 20:08:48 +0200

    page section indicator

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Navbar onscroll animation
 const nav = document.querySelector(".nav");
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 150) {
+function navScrollHandler(YOffset) {
+  if (YOffset > 150) {
     nav.classList.add("nav--scrolled");
   } else {
     nav.classList.remove("nav--scrolled");
   }
-});
+}
 
 // Show mobile menu.
 const btn = document.querySelector(".nav-mobile__button");
@@ -24,10 +24,42 @@ links.forEach(link => {
 });
 
 // Current section indicator
-const hero = document.querySelector(".hero").offsetTop;
-const about = document.querySelector(".about").offsetTop;
-const services = document.querySelector(".services").offsetTop;
-const gallery = document.querySelector(".gallery").offsetTop;
-const blog = document.querySelector(".blog").offsetTop;
-const contact = document.querySelector(".contact").offsetTop;
-console.log(about);
+const navItems = document.querySelectorAll(".nav__item");
+
+const hero = document.querySelector(".hero").offsetTop - 200;
+const about = document.querySelector(".about").offsetTop - 200;
+const services = document.querySelector(".services").offsetTop - 200;
+const gallery = document.querySelector(".gallery").offsetTop - 200;
+const blog = document.querySelector(".blog").offsetTop - 200;
+const contact = document.querySelector(".contact").offsetTop - 200;
+
+function activateNavItem(index) {
+  navItems.forEach(item => item.classList.remove("nav__item--active"));
+  navItems[index].classList.add("nav__item--active");
+}
+
+// Scroll handle
+window.addEventListener("scroll", () => {
+  let offset = window.pageYOffset;
+  navScrollHandler(offset);
+
+  switch (true) {
+    case offset >= about && offset < services:
+      activateNavItem(1);
+      break;
+    case offset >= services && offset < gallery:
+      activateNavItem(2);
+      break;
+    case offset >= gallery && offset < blog:
+      activateNavItem(3);
+      break;
+    case offset >= blog && offset < contact:
+      activateNavItem(4);
+      break;
+    case offset >= contact:
+      activateNavItem(5);
+      break;
+    default:
+      activateNavItem(0);
+  }
+});
