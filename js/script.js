263e2998f31549755a552fe5a1a9eba7b4a03197
lukasz <lorzechowski@protonmail.com>
2019-09-17 16:45:33 +0200

    mobile ready

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-// Navbar scroll
+// Navbar onscroll animation
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 150) {
+    nav.classList.add("nav--scrolled");
+  } else {
+    nav.classList.remove("nav--scrolled");
+  }
+});
+
+// Show mobile menu.
+const btn = document.querySelector(".nav-mobile__button");
+const menu = document.querySelector(".nav__list");
+btn.addEventListener("click", () => {
+  menu.classList.toggle("nav__list--active");
+});
+
+// Hide mobile menu
+const links = document.querySelectorAll(".nav__link");
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("nav__list--active");
+  });
+});
+
+// Current section indicator
+const hero = document.querySelector(".hero").offsetTop;
+const about = document.querySelector(".about").offsetTop;
+const services = document.querySelector(".services").offsetTop;
+const gallery = document.querySelector(".gallery").offsetTop;
+const blog = document.querySelector(".blog").offsetTop;
+const contact = document.querySelector(".contact").offsetTop;
+console.log(about);
